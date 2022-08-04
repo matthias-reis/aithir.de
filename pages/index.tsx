89ae3f52da3ae533e7f2561c316c833cf5c14434
matthias.reis <matthias.reis@xing.com>
2022-08-04 11:35:55 +0200

    restart with next js and vercel deployment

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import styled from '@emotion/styled';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Box = styled.div`
+  border: 10px dotted red;
+  padding: 3rem;
+`;
+
+const Home: NextPage = () => {
+  return <Box>Hello</Box>;
+};
+
+export default Home;
