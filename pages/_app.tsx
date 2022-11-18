9429e6e45951900012f5a2542ae5cc7dace6f985
matthias.reis <matthias.reis@xing.com>
2022-11-18 12:08:42 +0100

    Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app';
 import { TrackingProvider } from '../components/tracking-provider';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <TrackingProvider />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
