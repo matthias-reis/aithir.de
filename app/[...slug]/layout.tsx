dd6447cd4ab2d4f7e1534ec2518fb763d5fb0366
matthias.reis <matthias.reis@xing.com>
2023-08-20 18:39:56 +0200

    migrate hermetics storyline

--- app/[...slug]/layout.tsx
+++ app/[...slug]/layout.tsx
@@ -2,9 +2,9 @@ import { usePathname, useRouter } from 'next/navigation';
 import { headers } from 'next/headers';
 import { LayoutFrame } from '../../comp/layout-frame';
 import { getItem } from '../../core/data-layer';
-import { DynamicPageProps, FCC } from '../../core/types';
+import { FC, ReactNode } from 'react';
 
-const Layout: FCC = ({ children }) => {
+const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   const requestHeaders = headers();
   const slug = requestHeaders.get('x-path');
   const colorSpace = getItem(slug || '')?.colorSpace ?? 'neutral';
